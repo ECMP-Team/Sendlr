1cee2edd730a4d15cf3c0b11614c9d4650882b0b
YasserDalali <yasser.dalali.personal@gmail.com>
2025-04-27 20:07:35 +0100

    Implement campaign creation functionality and enhance user management in prismaUtils; update app.js for campaign route and add new logging features.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import express from 'express';
-import emailController from './controllers/emailController.js';
-import { healthCheck } from './controllers/healthController.js';
+import express from "express";
+import emailController from "./controllers/emailController.js";
+import { healthCheck } from "./controllers/healthController.js";
 
 const app = express();
 
 // Add middleware to parse JSON
 app.use(express.json({ limit: "10mb" }));
-// Email endpoints 
+// Email endpoints
 app.post("/api/send-bulk", emailController.sendBulkEmails);
-app.post("/api/send-individual", emailController.sendIndividualEmails);
+app.post("/api/send-individual", emailController.sendIndividualEmails); // will be used for resending individual failed emails or testing
 app.post("/api/generate-and-send", emailController.generateAndSendEmails);
 
+
+//TODO : Add a route to create a campaign in the database
+// This is a placeholder route, you should implement the actual logic in the controller
+app.post("/api/createCampaign", () => {
+  console.log("Campaign created");
+});
 // Health check endpoint
 app.get("/api/health", healthCheck);
 
