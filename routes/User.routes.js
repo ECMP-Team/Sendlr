0c2cab9ac0d0f2ac087a32035f15181ca9f5c7be
YasserDalali <yasser.dalali.personal@gmail.com>
2025-05-02 01:29:18 +0100

    ✨ fix(user): fixed user registration and login routes

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import UserController from "../controllers/userController.js";
-import CampaignController from "../controllers/campaignController.js";
 import authMiddleware from "../middleware/auth.middleware.js";
 const userRouter = Router();
 
-userRouter.get("/get-all-campaigns", authMiddleware, CampaignController.getAllCampaigns); // only allow user to get their own campaigns
-userRouter.get("/get-campaigns/:id", authMiddleware, CampaignController.getCampaign); // only allow user to get their own campaigns
+
 userRouter.get("/get-user/", authMiddleware, UserController.getUser); // self user
 userRouter.put("/update-user", authMiddleware, UserController.updateUser); // self user
+userRouter.post("/register-user", UserController.registerUser);
+userRouter.post("/login-user", UserController.loginUser);
 //! userRouter.get("/get-all-users/", authMiddleware, UserController.getAllUsers);
 //! userRouter.delete("/delete-user/:id", authMiddleware, UserController.deleteUser);
 
